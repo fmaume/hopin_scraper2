992cbd02b89f5adfa758ac91756b12dbaf7fd212
Fabian Maume <fabian.maume@laposte.net>
2020-06-10 21:17:24 +0200

    Update console.script.js

--- console.script.js
+++ console.script.js
@@ -1,6 +1,8 @@
 
 // scroll all attendees
 document.querySelector(".people-list_load-more__2dx_z").click()
+
+
 var i;
 for (i = 0; i < 40; i++) { 
 setTimeout(() => { document.querySelector(".people-list_load-more__2dx_z").click() }, 2000);
@@ -17,3 +19,9 @@ stemp.forEach(element => Name.push(element.innerText))
 
 stemp = document.querySelectorAll(".test-id-attendee-headline")
 stemp.forEach(element => Headline.push(element.innerText))
+
+Name.forEach(element => console.log(element))
+
+
+Headline.forEach(element => console.log(element))
+
