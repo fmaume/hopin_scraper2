7782ebc1d6f287532940fdecd3a8dcc1fc74a7db
Fabian Maume <fabian.maume@laposte.net>
2020-06-10 21:14:13 +0200

    Create console.script.js

---
+++ console.script.js
@@ -0,0 +1,19 @@
+
+// scroll all attendees
+document.querySelector(".people-list_load-more__2dx_z").click()
+var i;
+for (i = 0; i < 40; i++) { 
+setTimeout(() => { document.querySelector(".people-list_load-more__2dx_z").click() }, 2000);
+}
+
+
+
+//get attendee data
+Name = []
+Headline = []
+
+stemp = document.querySelectorAll(".test-id-attendee-name")
+stemp.forEach(element => Name.push(element.innerText))
+
+stemp = document.querySelectorAll(".test-id-attendee-headline")
+stemp.forEach(element => Headline.push(element.innerText))
